0e79e16490c72c8eaf843fecfdb9d9e5bea6b440
m208 <avcvet@yandex.ru>
2022-12-27 13:29:52 +0300

    feat: add http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world');
+import dotenv from 'dotenv';
+import http from 'http';
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      data: 'Hello World!'
+    }));
+  });
+
+server.listen(PORT);
+console.log(`Server started on port: ${PORT}`);
+
+
