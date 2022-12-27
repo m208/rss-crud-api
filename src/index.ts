a68ccacf3302fad2615bd0105d107344820e2ecb
m208 <avcvet@yandex.ru>
2022-12-27 16:11:46 +0300

    feat: add routing

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,32 @@ import http from 'http';
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-      data: 'Hello World!'
-    }));
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    const url = ((request.url || '')  + '/').replace('//', '/').toLowerCase();
+
+    const usersRouteRegexp =/\/api\/users\/(.*)/;
+    const usersRouteMatch = url.match(usersRouteRegexp);
+
+    let message = {};
+
+    if (usersRouteMatch){
+        const id = usersRouteMatch[1].replace('/', '');
+        message = { 
+            data: id? `one user with ID ${id}` : 'All users'
+        }
+    } else {
+        message = {
+            data: 'page not found',
+        };
+    }
+
+    response.end(JSON.stringify(message));
+
   });
+
+
 
 server.listen(PORT);
 console.log(`Server started on port: ${PORT}`);
-
-
